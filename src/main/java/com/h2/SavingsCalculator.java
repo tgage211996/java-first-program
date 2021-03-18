95e957002a90b6e62f782ed60340f4d56be4499f
Thomas Gage <57278497+tgage211996@users.noreply.github.com>
2021-03-18 14:57:01 -0400

    finished module 3

---
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -0,0 +1,66 @@
+package com.h2;
+
+import java.time.LocalDate;
+import java.time.YearMonth;
+
+public class SavingsCalculator {
+
+    private float[] credits;
+    private float[] debits;
+
+    public SavingsCalculator(float[] credits, float[] debits) {
+        this.credits = credits;
+        this.debits = debits;
+    }
+    private float sumOfCredits(){
+        float sum = 0.0f;
+        for(float num: credits){
+            sum += num;
+        }
+        return sum;
+    }
+
+    private float sumOfDebits(){
+        float sum = 0.0f;
+        for(float num: debits){
+            sum += num;
+        }
+        return sum;
+    }
+
+    private static int remainingDaysInMonth(LocalDate date){
+        YearMonth yearMonth = YearMonth.of(date.getYear(),date.getMonth());
+        int totalDaysInMonth = yearMonth.lengthOfMonth();
+
+        int remainingDays = totalDaysInMonth - date.getDayOfMonth();
+
+        return remainingDays;
+    }
+
+    public float calculate(){
+       return  sumOfCredits() - sumOfDebits();
+    }
+
+    public static void main(String[] args) {
+
+
+        String[] creditsAsString = args[0].split(",");
+        String[] debitsAsString =  args[1].split(",");
+
+        float[] credits = new float[creditsAsString.length];
+        float[] debits = new float[debitsAsString.length];
+
+        SavingsCalculator calculator = new SavingsCalculator(credits,debits);
+
+        for(int i = 0; i < creditsAsString.length; i++){
+            credits[i] = Float.parseFloat(creditsAsString[i]);
+        }
+
+        for(int i = 0; i < debitsAsString.length; i++){
+            debits[i] = Float.parseFloat(debitsAsString[i]);
+        }
+
+        float netSavings = calculator.calculate();
+
+    }
+}
